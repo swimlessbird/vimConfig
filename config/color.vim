b458cacb3e3fc56c442864d28c74f19182170979
swimlessbird <swimlessbird@gmail.com>
2022-01-06 19:21:01 +0800

    remove install global

--- config/color.vim
+++ config/color.vim
@@ -1,4 +1,7 @@
 set background=dark
 
+" 主题
+" 需要在/etc/profile中新增 export TERM=xterm-256color
+
 set t_CO=256
 colorscheme zenburn
--- install.sh
+++ install.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# install global
-if [ ! -f /usr/local/bin/gtags ] && [ ! -f /usr/bin/gtags ]
-then
-    cd global
-    configure
-    make && make install
-    cd -
-fi
-
-if [ ! -f ~/.vim ]
+if [ -f ~/.vim ]
 then
     mv ~/.vim ~/.vim_bak
-    ln -s `pwd` ~/.vim
 fi
+ln -s `pwd` ~/.vim
 
 if [ -L ~/.vimrc ]
 then
